87076bb0ef48329560ddc8cf445074ef16252e2d
Anteskera <ante.teskera@gmail.com>
2022-12-11 00:28:13 +0100

    fixes to import script

--- import_script.py
+++ import_script.py
@@ -8,6 +8,17 @@ db_user = 'postgres'
 db_password = '123'
 
 
+def get_positions_from_data(data):
+    comma_split = data.split(',')
+    slash_split = data.split('/')
+
+    position_split = slash_split if len(slash_split) >= len(comma_split) else comma_split
+    result = position_split[0].strip()
+    for pos in position_split[1:]:
+        result = result + ', ' + pos.strip()
+    return result
+
+
 def formatted_date(unformatted):
     split_date = unformatted.split('.')
     day = split_date[0]
@@ -45,9 +56,10 @@ for line in csv_file.readlines()[1:]:
         date = datetime.strptime(formatted_date(split[2]), '%Y-%m-%d')
         date = date.strftime("%Y-%m-%d")
         age = int(split[3])
+        positions = get_positions_from_data(split[6])
 
-        tup.append((split[0], split[1], date, age, split[4],
-                    split[5], split[6], split[7], split[8], split[11], split[12]))
+        tup.append((split[0], split[1], date, age, split[4].upper(), split[5].upper(), positions.upper(),
+                    split[7].upper(), split[8].upper(), split[11], split[12].upper()))
 
     except (ValueError, IndexError) as e:
         print(e)
@@ -59,7 +71,14 @@ cur = conn.cursor()
 
 cur.executemany("INSERT INTO player (name, full_name, date_of_birth, age, city_of_birth, "
                 "country_of_birth, positions, current_club, national_team, id, url)"
-                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tup)
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                "ON CONFLICT (url) DO UPDATE SET "
+                "(name, full_name, date_of_birth, age, city_of_birth, positions, country_of_birth, current_club, "
+                "national_team, id, url) = "
+                "(EXCLUDED.name, EXCLUDED.full_name, EXCLUDED.date_of_birth, EXCLUDED.age, "
+                "EXCLUDED.city_of_birth, EXCLUDED.positions, EXCLUDED.country_of_birth, EXCLUDED.current_club, "
+                "EXCLUDED.national_team, EXCLUDED.id, EXCLUDED.url)", tup)  # maknut visak jer si glup
 conn.commit()
 cur.close()
 conn.close()
+#Name;Full name;Date of birth;Age;City of birth;Country of birth;Position;Current club;National_team;Dead;No data;PlayerID;URL
